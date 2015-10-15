42dcb2e2dc36cad0ad489a364f4f05fe14746414
pollitosabroson <karnal02@gmail.com>
2015-10-14 19:35:11 -0500

    tercer commit

--- src/angular/static/app/js/libros/controllers.js
+++ src/angular/static/app/js/libros/controllers.js
@@ -1,8 +1,7 @@
 demo.controller('LibrosCtrl',
     function($scope, $api, Libros) {
-    	console.log($api.url(''))
-        $scope.books = Libros.lista();
-        console.log(Libros.lista())
+
+        $scope.libros = Libros.lista();
 
     }
 );
--- src/libros/serializers.py
+++ src/libros/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from .models import Libro
+from autores.serializers import AutorSerializer
 
 
 class LibroSerializer(serializers.ModelSerializer):
+    autor = AutorSerializer()
 
     class Meta:
         model = Libro
